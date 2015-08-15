92dd74dfbe30f8bf0d20c42608861c0f655c8225
Galileo Guzmán <guzgali@gmail.com>
2015-08-15 12:21:30 -0500

    Se accede al valor de la fila

--- data.py
+++ data.py
@@ -4,3 +4,19 @@ book = xlrd.open_workbook("farm_gdl.xls")
 
 print "The number of worksheets is", book.nsheets
 print "Worksheet name(s):", book.sheet_names()
+
+
+
+## Imprimir el objeto de la hoja de excel seleccionada
+sheet = book.sheet_by_index(0)
+print sheet
+
+## Imprimir el numero de columnas
+number_col = sheet.ncols
+print number_col
+
+for row in range(sheet.nrows):
+	if sheet.cell_value(rowx=row, colx=0) == '':
+		print 'CADENA VACIA POR GALILEO'
+	else:
+		print sheet.cell_value(rowx=row, colx=0)

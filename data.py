9b79119d056d8ef39e087f33f441762ca2bc6bbb
Galileo Guzmán <guzgali@gmail.com>
2015-08-15 12:44:49 -0500

    Cambio a openxlsx

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-import xlrd
+import xlrd, xlwt, xlutils
 
-book = xlrd.open_workbook("farm_gdl.xls")
+book = xlrd.open_workbook("farm_gdl.xls", formatting_info=True)
 
 print "The number of worksheets is", book.nsheets
 print "Worksheet name(s):", book.sheet_names()
@@ -17,6 +17,9 @@ print number_col
 
 for row in range(sheet.nrows):
 	if sheet.cell_value(rowx=row, colx=0) == '':
-		print 'CADENA VACIA POR GALILEO'
+		sheet._cell_values(row, 0) = 'cadena vacia por Galileo'
 	else:
 		print sheet.cell_value(rowx=row, colx=0)
+
+book.save('farm_gdl.xls')
+
